0fc9054cd555b615b790333ec39c0b15d97ad76d
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-05-16 23:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //your JS code here. If required.
+const select=document.getElementById("coloSelect");
+select.addEventListener("change",deletElement)
 function deletElement() {
-	const select=document.getElementById("coloSelect"){
-		select.remove(select.selectedIndex);
+	let index=select.selectedIndex;
+	if(index>=0){
+		select.remove(index)
 	}
+	
+		
 	
 	
 }
