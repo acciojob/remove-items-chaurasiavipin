dfc8b682ea8c7bc347574061a34229b34175126b
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-05-16 22:42:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function deletElement() {
-	const select=document.getElementById(colorSelect){
+	const select=document.getElementById("coloSelect"){
 		select.remove(select.selectedIndex);
 	}
 	
